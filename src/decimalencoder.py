f5dd0caee7be08d3f2dea08633d00bf0d981cee9
Ubuntu <ubuntu@ip-172-31-0-209.ec2.internal>
2023-01-11 19:24:07 +0000

    se añaden comentarios

--- src/decimalencoder.py
+++ src/decimalencoder.py
@@ -1,10 +1,12 @@
+#Importacion de librerias
 import decimal
 import json
 
-
+#marca un error en la linea 8
 # This is a workaround for: http://bugs.python.org/issue16535
 class DecimalEncoder(json.JSONEncoder):
-    def default(self, obj):
+    def default(self, obj):          # pylint: disable=E0202
+                                     # pylint: enable=E0202
         if isinstance(obj, decimal.Decimal):
             return int(obj)
         return super(DecimalEncoder, self).default(obj)

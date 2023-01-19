4205914eadfc4b45f005825f47f52f021496ee08
lgparedes <121614909+lgparedes@users.noreply.github.com>
2023-01-19 17:39:55 +0100

    Update decimalencoder.py

--- src/decimalencoder.py
+++ src/decimalencoder.py
@@ -6,8 +6,7 @@ import json
 # This is a workaround for: http://bugs.python.org/issue16535
 
 class DecimalEncoder(json.JSONEncoder):
-    def default(self, obj):          # pylint: disable=E0202
-                                     # pylint: enable=E0202
+    def default(self, obj):          
         if isinstance(obj, decimal.Decimal):
             return int(obj)
         return super(DecimalEncoder, self).default(obj)

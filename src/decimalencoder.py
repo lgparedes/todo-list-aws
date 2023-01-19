bedc66622963268e544da95f499a359c9af8ba5f
lgparedes <121614909+lgparedes@users.noreply.github.com>
2023-01-19 17:46:24 +0100

    Update decimalencoder.py

--- src/decimalencoder.py
+++ src/decimalencoder.py
@@ -1,11 +1,8 @@
-# Importacion de librerias
-
 import decimal
 import json
 
-# marca un error en la linea 8
-# This is a workaround for: http://bugs.python.org/issue16535
 
+# This is a workaround for: http://bugs.python.org/issue16535
 class DecimalEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, decimal.Decimal):

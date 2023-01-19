9a254c288d4032c55bbf380939799ff9c9ba9031
lgparedes <121614909+lgparedes@users.noreply.github.com>
2023-01-19 17:34:21 +0100

    Update decimalencoder.py

--- src/decimalencoder.py
+++ src/decimalencoder.py
@@ -4,6 +4,7 @@ import json
 
 # marca un error en la linea 8
 # This is a workaround for: http://bugs.python.org/issue16535
+
 class DecimalEncoder(json.JSONEncoder):
     def default(self, obj):          # pylint: disable=E0202
                                      # pylint: enable=E0202
